073f1dcf2412eed1ba03582df078937bd40a7459
Nayeli <nayi_n13@hotmail.com>
2021-08-07 23:42:53 -0400

    feat: Se configuro el modulo de deducciones

--- backend/src/deductions/deductions.module.ts
+++ backend/src/deductions/deductions.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DeductionsController } from './controllers/deductions.controller';
+import { deductionSchema } from './schemas/deductions.schema';
 import { DeductionsService } from './services/deductions.service';
 
 @Module({
     controllers: [DeductionsController],
+    imports:[MongooseModule.forFeature([{ name: 'deductions', schema: deductionSchema }])],
     providers: [DeductionsService],
 })
 export class DeductionsModule {}
